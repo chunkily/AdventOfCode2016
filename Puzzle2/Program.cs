49a3e56b0baacdbce84c3b58815c3eac58dca846
Lee Chengkai <leechengkai@gmail.com>
2016-12-07 17:56:29 +0800

    Change algorithm to deal with arbitrary keypads

--- Puzzle2/Program.cs
+++ Puzzle2/Program.cs
@@ -14,11 +14,35 @@ namespace Puzzle2
             string inputFilePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "../../input.txt");
             string[] lines = File.ReadAllLines(inputFilePath);
 
-            int xPos = 1;
-            int yPos = 1;
-            int keyPadHeight = 3;
-            int keyPadWidth = 3;
-            KeyPad keyPad = new KeyPad(keyPadWidth,keyPadHeight);
+            //Dictionary<char, KeyPadNumber> KeyPad = new Dictionary<char, KeyPadNumber>();
+            ////                                    UP  DOWN  LEFT RIGHT
+            //KeyPad.Add('1', new KeyPadNumber('1', '1', '4', '1', '2'));
+            //KeyPad.Add('2', new KeyPadNumber('2', '2', '5', '1', '3'));
+            //KeyPad.Add('3', new KeyPadNumber('3', '3', '6', '2', '3'));
+            //KeyPad.Add('4', new KeyPadNumber('4', '1', '7', '4', '5'));
+            //KeyPad.Add('5', new KeyPadNumber('5', '2', '8', '4', '6'));
+            //KeyPad.Add('6', new KeyPadNumber('6', '3', '9', '5', '6'));
+            //KeyPad.Add('7', new KeyPadNumber('7', '4', '7', '7', '8'));
+            //KeyPad.Add('8', new KeyPadNumber('8', '5', '8', '7', '9'));
+            //KeyPad.Add('9', new KeyPadNumber('9', '6', '9', '8', '9'));
+
+            Dictionary<char, KeyPadNumber> KeyPad = new Dictionary<char, KeyPadNumber>();
+            //                                    UP  DOWN  LEFT RIGHT
+            KeyPad.Add('1', new KeyPadNumber('1', '1', '3', '1', '1'));
+            KeyPad.Add('2', new KeyPadNumber('2', '2', '6', '2', '3'));
+            KeyPad.Add('3', new KeyPadNumber('3', '1', '7', '2', '4'));
+            KeyPad.Add('4', new KeyPadNumber('4', '4', '8', '3', '4'));
+            KeyPad.Add('5', new KeyPadNumber('5', '5', '5', '5', '6'));
+            KeyPad.Add('6', new KeyPadNumber('6', '2', 'A', '5', '7'));
+            KeyPad.Add('7', new KeyPadNumber('7', '3', 'B', '6', '8'));
+            KeyPad.Add('8', new KeyPadNumber('8', '4', 'C', '7', '9'));
+            KeyPad.Add('9', new KeyPadNumber('9', '9', '9', '8', '9'));
+            KeyPad.Add('A', new KeyPadNumber('A', '6', 'A', 'A', 'B'));
+            KeyPad.Add('B', new KeyPadNumber('B', '7', 'D', 'A', 'C'));
+            KeyPad.Add('C', new KeyPadNumber('C', '8', 'C', 'B', 'C'));
+            KeyPad.Add('D', new KeyPadNumber('D', 'B', 'D', 'D', 'D'));
+
+            KeyPadNumber currentNumber = KeyPad['5'];
 
             foreach(var line in lines)
             {
@@ -27,64 +51,47 @@ namespace Puzzle2
                     // Read the character and change the current position.
                     if(c=='U')
                     {
-                        yPos += 1;
+                        currentNumber = KeyPad[currentNumber.Up];
                     }
                     else if(c=='D')
                     {
-                        yPos -= 1;
+                        currentNumber = KeyPad[currentNumber.Down];
                     }
                     else if(c=='R')
                     {
-                        xPos += 1;
+                        currentNumber = KeyPad[currentNumber.Right];
                     }
                     else if(c=='L')
                     {
-                        xPos -= 1;
-                    }
-
-                    // Reset if out of bounds.
-                    if(xPos < 0)
-                    {
-                        xPos = 0;
-                    }
-                    if(xPos >= keyPadWidth)
-                    {
-                        xPos = keyPadWidth - 1;
-                    }
-                    if(yPos < 0)
-                    {
-                        yPos = 0;
-                    }
-                    if(yPos >= keyPadHeight)
-                    {
-                        yPos = keyPadHeight - 1;
+                        currentNumber = KeyPad[currentNumber.Left];
                     }
                     // DEBUG
-                    //Console.WriteLine(c + " " + xPos + " " + yPos + " " + keyPad.GetDisplayNumber(xPos, yPos));
+                    // Console.WriteLine(c + " " + currentNumber.DisplayNumber);
 
                 }
-                // Reached the end of a line, Get the number on the keypad
-                Console.WriteLine(keyPad.GetDisplayNumber(xPos,yPos));
+                // Reached the end of a line, output the number on the keypad.
+                Console.Write(currentNumber.DisplayNumber);
             }
 
             Console.ReadLine();
         }
     }
 
-    class KeyPad
+    class KeyPadNumber
     {
-        int keyPadWidth = 3;
-        int keyPadHeight = 3;
+        public char DisplayNumber { get; set; }
+        public char Up { get; set; }
+        public char Down { get; set; }
+        public char Left { get; set; }
+        public char Right { get; set; }
 
-        public int GetDisplayNumber(int xPos, int yPos)
+        public KeyPadNumber(char thisDisplay, char up, char down, char left, char right)
         {
-            return (keyPadHeight - yPos - 1) * keyPadWidth + xPos + 1;
-        }
-
-        public KeyPad(int width, int height)
-        {
-            this.keyPadWidth = width;
-            this.keyPadHeight = height;
+            this.DisplayNumber = thisDisplay;
+            this.Up = up;
+            this.Down = down;
+            this.Left = left;
+            this.Right = right;
         }
     }
 }
